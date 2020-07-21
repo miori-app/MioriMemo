e7b731ba0e32ed102a4f6774038cd15ad9a72693
iammiori <mioriii@naver.com>
2020-07-21 09:37:06 +0900

    Initial Commit

---
+++ MioriMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MioriMemo
+//
+//  Created by miori Lee on 2020/07/21.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
